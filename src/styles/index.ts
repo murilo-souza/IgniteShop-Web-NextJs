de61ceb95bbcb5236f73a5a0ccd5635e20624cac
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-12-22 19:08:18 -0300

    update

---
+++ src/styles/index.ts
@@ -0,0 +1,9 @@
+import { createStitches } from '@stitches/react'
+
+export const { config, styled, globalCss, css, keyframes, getCssText, theme, createTheme } = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#fe4567'
+    }
+  }
+})
