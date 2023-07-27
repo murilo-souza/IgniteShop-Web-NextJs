72f7e5eb145b2e2c35c355f637da897872b7bf58
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-07-27 17:21:42 -0300

    Starting ignite challenge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { globalStyles } from "../styles/global"
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from "../styles/pages/app"
 import Image from "next/image"
+import { Handbag } from "phosphor-react"
+import { theme } from "../styles"
 
 
 globalStyles()
@@ -12,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
+        <Handbag size={24} color="#FFFF"/>
       </Header>
 
       <Component {...pageProps} />
